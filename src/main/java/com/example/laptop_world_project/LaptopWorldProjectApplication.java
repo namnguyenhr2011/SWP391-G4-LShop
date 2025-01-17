fa3829c16b6274895906d990980d459686f015be
Nam <namnguyenhr2011@gmail.com>
2025-01-17 14:44:01 +0700

    Add form

---
+++ src/main/java/com/example/laptop_world_project/LaptopWorldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.laptop_world_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopWorldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopWorldProjectApplication.class, args);
+	}
+
+}
