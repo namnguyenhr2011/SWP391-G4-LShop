bc2b90d821b5e057aaa528c0f4f65d11dbfc10fb
Nam <namnguyenhr2011@gmail.com>
2025-03-07 13:36:30 +0700

    Update comment

--- Client/src/Store/reducer/cartReducer.js
+++ Client/src/Store/reducer/cartReducer.js
@@ -20,11 +20,11 @@ const cartSlice = createSlice({
             if (existingItem) {
                 existingItem.quantity += item.quantity || 1;
             } else {
-                state.items[userId].push({ 
-                    ...item, 
+                state.items[userId].push({
+                    ...item,
                     quantity: item.quantity || 1,
-                    originalPrice: item.originalPrice || item.price, // Store original price
-                    isSale: item.isSale || false // Store sale status
+                    originalPrice: item.originalPrice || item.price,
+                    isSale: item.isSale || false
                 });
             }
         },
