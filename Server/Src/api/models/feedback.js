3c7226e699619ca5851da4eae206294407b1ff54
HiuHi32 <nhhuy322003@gmail.com>
2025-03-14 12:46:33 +0700

    fix merge

---
+++ Server/Src/api/models/feedback.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const feedbackSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      required: false, 
+    },
+  },
+  { timestamps: true }
+);
+
+const Feedback = mongoose.model('Feedback', feedbackSchema);
+
+module.exports = Feedback;
